c54e95cfdc8d850b276f1a51821c2ed6d5596cd9
Don Heshanthaka <heshanthaka007@gmail.com>
2022-03-19 04:06:22 +0530

    Creation of Home Page widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 
 
@@ -10,8 +12,33 @@ void main() {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginView(),
+      home: const HomePage(),
     ),
   );
 }
 
+
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+   
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        ),
+        body: FutureBuilder(
+          future: Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState){
+              case ConnectionState.done:
+                return const Text('Done');
+              default:
+                return const Text('Loading...');
+            }
+          },
+        ),
+    );
+  }
+}
