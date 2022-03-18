87edf164c4aa0801298248f7ddce720a65d51a8b
Don Heshanthaka <heshanthaka007@gmail.com>
2022-03-19 04:31:24 +0530

    Basic check for email verification on the HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/firebase_options.dart';
@@ -33,6 +34,12 @@ class HomePage extends StatelessWidget {
           builder: (context, snapshot) {
             switch (snapshot.connectionState){
               case ConnectionState.done:
+                final user = FirebaseAuth.instance.currentUser;
+                if (user?.emailVerified ?? false){ // if user.emailVerified is true or false; ?? is used as an if else condition (left to the ?? being true)
+                  print('You need to verfiy your email first');
+                } else {
+                  print('You need to verify your email');
+                }
                 return const Text('Done');
               default:
                 return const Text('Loading...');
