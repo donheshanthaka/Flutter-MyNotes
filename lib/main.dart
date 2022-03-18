b6798fc0470cf6cea2b7e0b0a72e82d6fcb1e14d
Don Heshanthaka <heshanthaka007@gmail.com>
2022-03-18 21:55:29 +0530

    Implementation of firebase authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -44,23 +48,42 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("Register"),
         ),
-        body: Column(
-          children: [
-            TextField(
-              controller: _email,
-              decoration: const InputDecoration(hintText: "Email"),
-            ),
-            TextField(
-              controller: _password,
-              decoration: const InputDecoration(hintText: "Password"),
-            ),
-            TextButton(
-              onPressed: () async {
-                
-              },
-              child: const Text("Register"),
-            ),
-          ],
+        body: FutureBuilder(
+          future: Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState){
+              case ConnectionState.done:
+                return Column(
+                  children: [
+                    TextField(
+                      controller: _email,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(hintText: "Email"),
+                    ),
+                    TextField(
+                      controller: _password,
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration: const InputDecoration(hintText: "Password"),
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        final email = _email.text;
+                        final password = _password.text;
+                        final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+                        print(userCredential);
+                      },
+                      child: const Text("Register"),
+                    ),
+                  ],
+                );
+              default:
+                return const Text('Loading...');
+            }
+          },
         ),
     );
   }
