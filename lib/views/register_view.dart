3a16575f74820f191b90898f831749dffa433012
Don Heshanthaka <heshanthaka007@gmail.com>
2022-03-22 01:43:37 +0530

    Implementation of showErrorDialog in the register view

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
-
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/utilities/show_error_dialog.dart';
 
 
 
@@ -60,18 +59,38 @@ class _RegisterViewState extends State<RegisterView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                final userCredential = await FirebaseAuth.instance
-                    .createUserWithEmailAndPassword(
-                        email: email, password: password);
-                devtools.log(userCredential.toString());
+                await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                  email: email,
+                  password: password,
+                );
+                //Navigator.of(context).pushNamed(verifyEmailRoute);
               } on FirebaseAuthException catch (e) {
-                if (e.code == 'weak-password') {
-                  devtools.log('Weak password');
-                } else if (e.code == 'email-already-in-use') {
-                  devtools.log('Email is already in use');
-                } else if (e.code == 'invalid-email') {
-                  devtools.log('Invalid email entered');
+                if (e.code == 'weak-password') { // cathcing weak password
+                  await showErrorDialog(
+                    context,
+                    'Weak password',
+                  );
+                } else if (e.code == 'email-already-in-use') { // catching emails that are already in use
+                  await showErrorDialog(
+                    context,
+                    'Email is already in use',
+                  );
+                } else if (e.code == 'invalid-email') { // catching invalid emails
+                  await showErrorDialog(
+                    context,
+                    'Invalid email entered',
+                  );
+                } else {
+                  await showErrorDialog( // Catching other errors related to firebase authentication
+                    context,
+                    'Error ${e.code}',
+                  );
                 }
+              } catch (e) { // Catching generic errors
+                await showErrorDialog(
+                    context,
+                    e.toString(),
+                );
               }
             },
             child: const Text("Register"),
