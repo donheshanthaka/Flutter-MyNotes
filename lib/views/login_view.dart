a21d6224d77827579d971cd329bcd7821071af4b
Don Heshanthaka <heshanthaka007@gmail.com>
2022-03-21 20:51:32 +0530

    Creation of the showErrorDialog function

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -82,3 +82,27 @@ class _LoginViewState extends State<LoginView> {
     );
   }
 }
+
+
+Future<void> showErrorDialog(
+  BuildContext context,
+  String text,
+) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('An error occurred'),
+        content: Text(text),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('OK'),
+          )
+        ],
+      );
+    },
+  );
+}
