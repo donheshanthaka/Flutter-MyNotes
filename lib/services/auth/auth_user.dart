812eba1be9e0a8e1e423fccf8e1e385b39f5a9c0
Don Heshanthaka <heshanthaka007@gmail.com>
2022-03-23 09:32:36 +0530

    Implementation of unit testing to the AuthService functionality

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -4,8 +4,8 @@ import 'package:flutter/cupertino.dart';
 @immutable
 class AuthUser {
   final bool isEmailVerified;
-  const AuthUser(this.isEmailVerified);
+  const AuthUser({required this.isEmailVerified});
 
   // Calling the firebase auth user to get the email verification status (bool) and passing that value for the constructor in AuthUser class
-  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);
+  factory AuthUser.fromFirebase(User user) => AuthUser(isEmailVerified: user.emailVerified);
 }
