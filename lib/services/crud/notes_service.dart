b1ef84d71a0170c6a780b8696d1717cdd2eef1ca
Don Heshanthaka <heshanthaka007@gmail.com>
2022-03-24 23:10:42 +0530

    Implementation of the local storage through sqlite

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -1,4 +1,87 @@
+import 'package:flutter/cupertino.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:path_provider/path_provider.dart' show getApplicationDocumentsDirectory;
+import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' show join;
 
+
+class DatabaseAlreadyOpenException implements Exception {}
+class UnableToGetDocumentsDirectory implements Exception {}
+
+class NotesService {
+
+  Database? _db;
+
+  Future<void> open() async {
+    if (_db != null){ // to check if the database in open
+      throw DatabaseAlreadyOpenException;
+    }
+    try {
+      final docsPath = await getApplicationDocumentsDirectory();
+    } on MissingPlatformDirectoryException {
+      throw UnableToGetDocumentsDirectory();
+
+    }
+  }
+}
+
+@immutable
+class DatabaseUser {
+  final int id;
+  final String email;
+
+  const DatabaseUser({
+    required this.id,
+    required this.email,
+  });
+
+  DatabaseUser.fromRow(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        email = map[emailColumn] as String;
+
+  @override
+  String toString() => 'Person, ID = $id, email = $email';
+
+  @override
+  bool operator ==(covariant DatabaseUser other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+}
+
+class DatabaseNote {
+  final int id;
+  final int userId;
+  final String text;
+  final bool isSyncedWithCloud;
+
+  DatabaseNote({
+    required this.id,
+    required this.userId,
+    required this.text,
+    required this.isSyncedWithCloud,
+  });
+
+  DatabaseNote.fromRow(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        userId = map[userIdColumn] as int,
+        text = map[textColumn] as String,
+        isSyncedWithCloud = (map[isSyncedWithColumn] as int) == 1 ? true : false;
+
+  @override
+  String toString() => 'Note, ID = $id, userId = $userId, isSyncedWithCloud = $isSyncedWithCloud, text = $text';
+
+  @override
+  bool operator ==(covariant DatabaseNote other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+}
+
+const dbName = 'notes.db';
+const noteTable = 'note';
+const userTable = 'user';
+const idColumn = 'id';
+const emailColumn = 'email';
+const userIdColumn = 'user_id';
+const textColumn = 'text';
+const isSyncedWithColumn = 'is_synced_with_cloud';
