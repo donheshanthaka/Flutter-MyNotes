0ce72ab0aa86cfde4b70fc1d4b400ea14d5fbd57
Don Heshanthaka <heshanthaka007@gmail.com>
2022-03-28 18:51:40 +0530

    Addition of email string to the auth user

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -3,9 +3,16 @@ import 'package:flutter/cupertino.dart';
 
 @immutable
 class AuthUser {
+  final String? email;
   final bool isEmailVerified;
-  const AuthUser({required this.isEmailVerified});
+  const AuthUser({
+    required this.email,
+    required this.isEmailVerified,
+  });
 
   // Calling the firebase auth user to get the email verification status (bool) and passing that value for the constructor in AuthUser class
-  factory AuthUser.fromFirebase(User user) => AuthUser(isEmailVerified: user.emailVerified);
+  factory AuthUser.fromFirebase(User user) => AuthUser(
+        email: user.email,
+        isEmailVerified: user.emailVerified,
+      );
 }
