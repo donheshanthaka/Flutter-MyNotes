86d1bee7f3c409d19dcce1ae82d70d4051ee91fb
Don Heshanthaka <heshanthaka007@gmail.com>
2022-03-29 10:40:21 +0530

    Creation of the createNewNote function

--- lib/views/notes/new_note_view.dart
+++ lib/views/notes/new_note_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/services/crud/notes_service.dart';
 
 class NewNoteView extends StatefulWidget {
   const NewNoteView({ Key? key }) : super(key: key);
@@ -8,6 +10,26 @@ class NewNoteView extends StatefulWidget {
 }
 
 class _NewNoteViewState extends State<NewNoteView> {
+
+  DatabaseNote? _note;
+  late final NotesService _notesService;
+  late final TextEditingController _textController;
+
+  Future<DatabaseNote> createNewNote() async {
+    final existingNote = _note;
+    // Checked using the negated value of the overriden equality operator
+    if (existingNote != null){
+      return existingNote;
+    }
+    // A user is always created when the app gets to the notes view, so we are retrieving that user as current user 
+    // The '!' at the end means we are upwrapping the return value even if it is an option value, that means we are forcing the app to give us that value
+    final currentUser = AuthService.firebase().currentUser!;
+    final email = currentUser.email!;
+    final owner = await _notesService.getUser(email: email);
+    return await _notesService.createNote(owner: owner); 
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
