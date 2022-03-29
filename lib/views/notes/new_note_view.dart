89027be30be52607359414ff38699b2d046b5757
Don Heshanthaka <heshanthaka007@gmail.com>
2022-03-29 12:23:11 +0530

    Implementation of text input from the user in new note view

--- lib/views/notes/new_note_view.dart
+++ lib/views/notes/new_note_view.dart
@@ -3,32 +3,83 @@ import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/services/crud/notes_service.dart';
 
 class NewNoteView extends StatefulWidget {
-  const NewNoteView({ Key? key }) : super(key: key);
+  const NewNoteView({Key? key}) : super(key: key);
 
   @override
   State<NewNoteView> createState() => _NewNoteViewState();
 }
 
 class _NewNoteViewState extends State<NewNoteView> {
-
   DatabaseNote? _note;
   late final NotesService _notesService;
   late final TextEditingController _textController;
 
+  @override
+  void initState() {
+    // Initialize the notes service and text controller
+    _notesService = NotesService();
+    _textController = TextEditingController();
+    super.initState();
+  }
+
+  void _textControllerListener() async {
+    final note = _note;
+    if (note == null) {
+      return;
+    }
+    final text = _textController.text;
+    await _notesService.updateNote(
+      note: note,
+      text: text,
+    );
+  }
+
+  void _setupTextControllerListener() {
+    _textController.removeListener(_textControllerListener);
+    _textController.addListener(_textControllerListener);
+
+  }
+
   Future<DatabaseNote> createNewNote() async {
     final existingNote = _note;
     // Checked using the negated value of the overriden equality operator
-    if (existingNote != null){
+    if (existingNote != null) {
       return existingNote;
     }
-    // A user is always created when the app gets to the notes view, so we are retrieving that user as current user 
+    // A user is always created when the app gets to the notes view, so we are retrieving that user as current user
     // The '!' at the end means we are upwrapping the return value even if it is an option value, that means we are forcing the app to give us that value
     final currentUser = AuthService.firebase().currentUser!;
     final email = currentUser.email!;
     final owner = await _notesService.getUser(email: email);
-    return await _notesService.createNote(owner: owner); 
+    return await _notesService.createNote(owner: owner);
   }
 
+  void _deleteNoteIfTextIsEmpty() {
+    final note = _note;
+    if (_textController.text.isEmpty && note != null) {
+      _notesService.deleteNote(id: note.id);
+    }
+  }
+
+  void _saveNoteIfTextNotEmpty() async {
+    final note = _note;
+    final text = _textController.text;
+    if (note != null && text.isNotEmpty) {
+      await _notesService.updateNote(
+        note: note,
+        text: text,
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    // when the user press the back button, it will first check if the text is empty and if it is, it will be deleted, and then do a similar check to see if it is not empty and then save
+    _deleteNoteIfTextIsEmpty();
+    _saveNoteIfTextNotEmpty();
+    _textController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +87,28 @@ class _NewNoteViewState extends State<NewNoteView> {
       appBar: AppBar(
         title: const Text('New Note'),
       ),
-      body: const Text('Write your new note here...'),
+      body: FutureBuilder(
+        future: createNewNote(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState){
+            case ConnectionState.done:
+            // saving the created snapshot data to the local note variable of type DatabaseNote
+            _note = snapshot.data as DatabaseNote;
+            _setupTextControllerListener();
+              return TextField(
+                // Using the text controller created by us to get the user input from the text field
+                controller: _textController,
+                keyboardType: TextInputType.multiline,
+                // Making the text field multi line
+                maxLines: null,
+                decoration: const InputDecoration(
+                  hintText: 'Start typing your note...',
+                ),
+              );
+            default:
+              return const CircularProgressIndicator();
+          }
+        }),
     );
   }
 }
