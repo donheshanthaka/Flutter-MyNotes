99b9a339f8601bfefe6a67395f8193fb967c63ea
Don Heshanthaka <heshanthaka007@gmail.com>
2022-03-30 20:40:38 +0530

    Fixed the error of "'Null' is not a subtype of type 'DatabaseNote' in type cast" by adding a nul aware operator '?' after DatabaseNote in snapshot.data

--- lib/views/notes/new_note_view.dart
+++ lib/views/notes/new_note_view.dart
@@ -87,7 +87,26 @@ class _NewNoteViewState extends State<NewNoteView> {
       appBar: AppBar(
         title: const Text('New Note'),
       ),
-      body: const Text('Write your note here...'),
+      body: FutureBuilder(
+        future: createNewNote(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+            _note = snapshot.data as DatabaseNote?;
+            _setupTextControllerListener();
+              return TextField(
+                controller: _textController,
+                keyboardType: TextInputType.multiline,
+                maxLines:  null,
+                decoration: const InputDecoration(
+                  hintText: 'Start typing your note...'
+                ),
+              );
+            default:
+              return const CircularProgressIndicator();
+          }
+        },
+      ),
     );
   }
 }
