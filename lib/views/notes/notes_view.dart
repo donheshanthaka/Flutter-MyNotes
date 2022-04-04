57c9b57ed553e3ef0f0793d9e85b9fb8b8226591
Don Heshanthaka <heshanthaka007@gmail.com>
2022-04-04 15:42:13 +0530

    Displaying notes as a list on the screen

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -69,8 +69,17 @@ class _NotesViewState extends State<NotesView> {
                     case ConnectionState.active:
                       if (snapshot.hasData) {
                         final allNotes = snapshot.data as List<DatabaseNote>;
-                        print(allNotes);
-                        return const Text('Got all the notes');
+                        return ListView.builder(
+                          itemCount: allNotes.length, 
+                          itemBuilder: (context, index ){
+                            final note = allNotes[index];
+                            return ListTile(
+                              title: Text(
+                                note.text
+                              ),
+                            );
+                          },
+                        );
                       } else {
                         return const CircularProgressIndicator();
                       }
