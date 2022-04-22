c73965a56f268aa63567ec150ef7765f1ae9d916
Don Heshanthaka <heshanthaka007@gmail.com>
2022-04-22 18:13:26 +0530

    Removal of the show log out dialog method from notes view

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -100,26 +100,3 @@ class _NotesViewState extends State<NotesView> {
   }
 }
 
-Future<bool> showLogOutDialog(BuildContext context) {
-  return showDialog<bool>(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
-        title: const Text('Sign out'),
-        content: const Text('Are you sure you want to log out?'),
-        actions: [
-          TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(false);
-              },
-              child: const Text('Cancel')),
-          TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(true);
-              },
-              child: const Text('Logout'))
-        ],
-      );
-    },
-  ).then((value) => value ?? false);
-}
