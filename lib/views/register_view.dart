d6f8f41ccf927ee1fcda059319a5fccb471c64bc
Don Heshanthaka <heshanthaka007@gmail.com>
2022-04-25 13:50:02 +0530

    Implementation of error dialog in register view

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/services/auth/auth_exceptions.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
-import 'package:mynotes/utilities/show_error_dialog.dart';
+import '../utilities/dialogs/error_dialog.dart';
+
 
 
 
